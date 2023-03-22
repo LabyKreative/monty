cb7cfe5b3f3c2221f0a4af692ef083cf534a7a1b
LabyKreative <labykreative@yahoo.com>
2023-03-22 10:06:14 -0700

    Add node to the stack

--- 0-push.c
+++ 0-push.c
@@ -1,24 +1,37 @@
 #include "monty.h"
-
 /**
- * f_pop - prints the top
+ * f_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_pop(stack_t **head, unsigned int counter)
+void f_push(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
+	int n, j = 0, flag = 0;
 
-	if (*head == NULL)
+	if (bus.arg)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	*head = h->next;
-	free(h);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
